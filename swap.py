88be55d228f9981dea98371537f0d45e521fc0b9
Jordan "Shijo" Lefrançois <90909417+jordan0602@users.noreply.github.com>
2021-10-12 09:06:30 +0200

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,9 @@
+a = int(input("Quel est votre nombre a ?"))
+b = int(input("Quel est votre nombre b ?"))
+
+temp_1 = a
+a = b
+b = temp_1
+
+print(f" apres inversion a vaut : {a}")
+print(f" apres inversion b vaut : {b}")
