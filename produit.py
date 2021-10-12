4ac5af60d09988a07fe69f89ff2a854734608f23
Jordan "Shijo" Lefrançois <90909417+jordan0602@users.noreply.github.com>
2021-10-12 09:06:15 +0200

    Create produit.py

---
+++ produit.py
@@ -0,0 +1,6 @@
+answer = int(input("Quel est votre premier nombre ?\n"))
+print(f"{answer}")
+answer2 = int(input("Quel est votre deuxieme nombre ?\n"))
+print(f"{answer2}")
+answer3 = answer * answer2
+print(f"Le produit de vos deux nombres est de :", answer3)
